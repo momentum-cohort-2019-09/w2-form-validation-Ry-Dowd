7ce1de6cd47ec44c5df57406358779250c0dc1b9
Ryan <rydowd@gmail.com>
2019-09-19 11:01:41 -0400

    created toggle function for empty inputs

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log('Add validation!');
+let submit = document.querySelector("#submit-button")
+
+
+function validateForm(){
+    let inputs = document.querySelectorAll(".input-field")
+    console.log(inputs)
+    for(let i of inputs){
+        console.log("looping through "+i.value)
+        if (i.value === undefined){
+            i.classList.toggle("input-invalid")
+            console.log("toggling")
+        }
+    }
+}
+
+submit.addEventListener('click', function(){
+    validateForm()
+})
