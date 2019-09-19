536d9e42916aa256857a14dfc506f76a6a5e1e13
Ryan <rydowd@gmail.com>
2019-09-19 19:24:57 -0400

    pseudocoding for name

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 console.log('Add validation!');
-let submit = document.querySelector("#submit-button")
+const submit = document.querySelector("#parking-form")
 
+function validName(content){
+    for (let i of content){
+        // loop through content string and if any characters aren't letters or whitespace, return false)
+        if (i )
+    }
+}
 
 function validateForm(){
     let inputs = document.querySelectorAll(".input-field")
     console.log(inputs)
+    let trimmed
     for(let i of inputs){
         console.log("looping through "+i.value)
-        if (i.value === undefined){
+        trimmed = i.trim()
+        if (!i.value){
             i.classList.toggle("input-invalid")
             console.log("toggling")
         }
     }
 }
 
-submit.addEventListener('click', function(){
-    validateForm()
-})
+submit.addEventListener('submit',event => event.preventDefault())
